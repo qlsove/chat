959254b306dde2bed020f0a35ba368ccf9dc3593
vasyliuk_n <vasyliuk.n@ki-technology.ru>
2015-12-18 21:42:15 +0200

    fix

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -3,7 +3,7 @@
   <head>
     <meta http-equiv="Content-type" content="text/html; charset=UTF-8"/>
     <title><?=$title?></title>
-    <?=Site::favicon('assets/images/favicon.ico')?>
+    <?=Site::favicon('assets/images/favicon.png')?>
     <?php foreach($styles as $file) {echo HTML::style($file), "\n";}?>
     <?php foreach($scripts as $file) {echo HTML::script($file), "\n";}?>
   </head>
